3a4d9ee4b2b31e657770fb9abde72aff875450cf
RobotYkrop <dendozzor@gmail.com>
2022-11-21 15:34:40 +0500

    fix button

--- src/components/popup/popup.tsx
+++ src/components/popup/popup.tsx
@@ -3,8 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { NavLink, useParams } from 'react-router-dom';
 import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import { IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import { createSelector } from '@reduxjs/toolkit';
 
@@ -32,19 +30,19 @@ const Popup = () => {
   const { name, status, species, image, gender, type } = char;
   return ReactDOM.createPortal(
     <Card data-testid="popup" sx={{ minWidth: 275, position: 'absolute', top: '20px', right: '200px' }}>
-      <IconButton sx={{ position: 'absolute', right: 0 }} onClick={() => dispatch(modalOpen(false))}>
+      <button className={pop['close']} onClick={() => dispatch(modalOpen(false))}>
         <NavLink data-testid="close-popup" to={'/'}>
           <CloseIcon />
         </NavLink>
-      </IconButton>
+      </button>
       <h3 className={pop['name']}>{name}</h3>
       <ul className={pop['characteristics']}>
         <li>Status: {status}</li>
         <li>Species: {species}</li>
         <li>Gender: {gender}</li>
-        <li>Type: {type ? type : 'No type'}</li>
+        <li>Type: {type ? type : 'Not type'}</li>
       </ul>
-      <img loading="lazy" src={image} alt="avatar" />
+      <img className={pop['avatar']} loading="lazy" src={image} alt="avatar" />
     </Card>,
     appRoot
   );
